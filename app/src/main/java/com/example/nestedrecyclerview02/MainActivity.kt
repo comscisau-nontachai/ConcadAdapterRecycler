04c2e383322d63c4bc9d2c65864214a4d83f741b
nontachai_jaila <nontachai_j@feyverly.com>
2021-11-10 22:56:33 +0700

    fix image.

--- app/src/main/java/com/example/nestedrecyclerview02/MainActivity.kt
+++ app/src/main/java/com/example/nestedrecyclerview02/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.nestedrecyclerview02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.nestedrecyclerview02.adapter.AnimalSectionAdapter
+import java.security.AccessController.getContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,6 +33,15 @@ class MainActivity : AppCompatActivity() {
         findViewById<RecyclerView>(R.id.recyclerView).run {
             layoutManager = LinearLayoutManager(this@MainActivity,RecyclerView.VERTICAL,false)
             adapter = sectionAdapter
+        }
+    }
+
+    fun ImageView.loadImage(url: String) {
+        try {
+            Glide.with(context).load(url).diskCacheStrategy(DiskCacheStrategy.NONE)
+                .skipMemoryCache(true).placeholder(R.drawable.ic_launcher_background).into(this)
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 }
